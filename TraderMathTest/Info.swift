c2105289cc81a0418a869040b88d86c7a783623e
Milap Naik <milapnaik@gmail.com>
2017-03-22 00:51:09 -0400

    Minor Adjustments

--- TraderMathTest/Info.swift
+++ TraderMathTest/Info.swift
@@ -19,6 +19,8 @@ class InfoController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        InfoText.text = "This app is meant to help practice for the math portion of interviews with finance firms. Pick your difficulty setting and amount of questions for quick, randomized practice sessions; or go for the full test and see how you do. Enjoy!"
+        InfoText.textAlignment = .center
         
     }
     
