29c0f0b1983d186869631a6c0b3271c048a508d2
Milap Naik <milapnaik@gmail.com>
2022-07-30 22:36:03 -0400

    text to black

--- TraderMathTest/VC/Info.swift
+++ TraderMathTest/VC/Info.swift
@@ -21,6 +21,7 @@ class InfoController: UIViewController {
         super.viewDidLoad()
         InfoText.text = "This app is meant to help practice for the math portion of interviews with finance firms. Pick your difficulty setting and amount of questions for quick, randomized practice sessions; or go for the full test and see how you do. Enjoy!"
         InfoText.textAlignment = .center
+        InfoText.textColor = UIColor.black
         
     }
     
