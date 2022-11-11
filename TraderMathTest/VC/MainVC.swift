c29087ab28fc162dc05acc31ae102fb32c58211a
Bhavesh Gupta <bhavesh.bansal@outlook.com>
2022-11-12 00:12:24 +0530

    major changes [WIP]
    - code refactored
    - added helper methods
    - added color assets
    - complain main vc

--- TraderMathTest/VC/MainVC.swift
+++ TraderMathTest/VC/MainVC.swift
@@ -9,24 +9,121 @@
 import UIKit
 import AppTrackingTransparency
 import AdSupport
+import DLRadioButton
 
+enum Test {
+    
+    enum Level: String {
+        case easy = "easy"
+        case medium = "medium"
+        case hard = "hard"
+    }
+    enum Count: Int {
+        case five = 5
+        case ten = 10
+        case fifteen = 15
+    }
+    enum TType: String {
+        case practice = "Practice"
+        case test = "test"
+    }
+}
 class MainVC: UIViewController {
     
-    // MARK: Properties
+    // MARK: Constants
     let preferences = UserDefaults.standard
-    let testtypeKey = "TestType"
-    var testType:String?
+    let difficultyKey = "Difficulty"
+    let questionnumKey = "QuestionNum"
+    let PoTKey = "PoT"
+    
+    // MARK: Properties
+    private let testtypeKey = "TestType"
+    private var testType:String?
+    private var difficulty: String = "easy"
     
     //MARK: IBOutlets
-    @IBOutlet weak var Settings: UIButton!
-    @IBOutlet weak var Info: UIButton!
+    @IBOutlet weak var sequenceTestView: UIView!
+    @IBOutlet weak var mathTestView: UIView!
+    @IBOutlet weak var practiceBtn: UIButton!
+    @IBOutlet weak var testBtn: UIButton!
+    @IBOutlet weak var startBtn: UIButton!
+    @IBOutlet weak var bottomSelectionView: UIView!
+    @IBOutlet weak var settingsView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewSetup()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        sequenceTestView.dropShadow()
+        mathTestView.dropShadow()
+        practiceBtn.roundedBorders(radius: 4)
+        testBtn.roundedBorders(radius: 4)
+        startBtn.roundedBorders(radius: 4)
+        settingsView.dropShadow()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         requestIDFA()
+    }
+    
+    // MARK: IBActions
+    @IBAction func practiceClicked() {
+        preferences.set(Test.TType.practice.rawValue, forKey: PoTKey)
+        showSettingsView()
+    }
+    
+    @IBAction func testClicked() {
+        preferences.set(Test.TType.test.rawValue, forKey: PoTKey)
+        showSettingsView()
+    }
+    
+    @IBAction func questionsCountClicked(_ sender: DLRadioButton) {
+        guard let intVal = Int(sender.titleLabel!.text!) else { return }
+        let qCount = Test.Count(rawValue: intVal)
+        preferences.set(qCount!.rawValue, forKey: questionnumKey)
+    }
+    
+    @IBAction func levelDifficultyClicked(_ sender: DLRadioButton) {
+        guard let senderString = sender.titleLabel?.text?.lowercased() else { return }
+        let level = Test.Level(rawValue: senderString)
+        preferences.set(level!.rawValue, forKey: difficultyKey)
+    }
+    
+    @IBAction func startClicked() {
+        print("Start Test")
+    }
+    
+    // MARK: View
+    func viewSetup() {
+        self.addGestureToViews()
+    }
+    
+    func showBottomSelectionView() {
+        bottomSelectionView.isHidden = false
+    }
+    
+    func showSettingsView() {
+        bottomSelectionView.isHidden = true
+        settingsView.isHidden = false
+    }
+    
+    // MARK: Gesture
+    func addGestureToViews() {
+        let tapGestureForSequence = UITapGestureRecognizer(target: self, action: #selector(MainVC.sequenceClicked))
+        sequenceTestView.addGestureRecognizer(tapGestureForSequence)
+        
+        let tapGestureForMathTest = UITapGestureRecognizer(target: self, action: #selector(MainVC.mathTestClicked))
+        mathTestView.addGestureRecognizer(tapGestureForMathTest)
+    }
+    
+    @objc func sequenceClicked() {
+        showBottomSelectionView()
+    }
+    
+    @objc func mathTestClicked() {
+        showBottomSelectionView()
     }
     
     // MARK: Segue
