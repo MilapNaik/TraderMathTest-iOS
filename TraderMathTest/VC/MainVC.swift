2ebdb5fae4cf419cbd27cc83e3e5147aa553349a
Bhavesh Gupta <bhavesh.bansal@outlook.com>
2023-01-22 13:22:43 +0530

    extracted model in separate class

--- TraderMathTest/VC/MainVC.swift
+++ TraderMathTest/VC/MainVC.swift
@@ -12,51 +12,6 @@ import AdSupport
 import DLRadioButton
 import FirebaseDatabase
 
-enum Test: CaseIterable {
-    
-    enum ScoreType: String, CaseIterable {
-        case local = "local"
-        case global = "global"
-    }
-    
-    enum Level: String, CaseIterable  {
-        case easy = "easy"
-        case medium = "medium"
-        case hard = "hard"
-    }
-    
-    enum Count: Int, CaseIterable  {
-        case five = 5
-        case ten = 10
-        case fifteen = 15
-        case fifty = 50
-        case eighty = 80
-    }
-    
-    enum TType: String, CaseIterable  {
-        case practice = "practice"
-        case test = "test"
-    }
-    
-    enum Category: String, CaseIterable  {
-        case math = "math"
-        case sequence = "sequence"
-    }
-    
-    enum Key: String, CaseIterable {
-        case DIFFICULTY_KEY = "Difficulty_Key"
-        case QUESTNUM_KEY = "Question_Num_Key"
-        case POT_KEY = "PoT_Key"
-        case TEST_TYPE_KEY = "Test_Type_Key"
-        case SETTINGS_SAVED_KEY = "SETTINGS_SAVED_KEY"
-        
-        var val : String {
-            return self.rawValue
-        }
-    }
-    
-}
-
 class MainVC: BaseVC {
     
     // MARK: Constants
